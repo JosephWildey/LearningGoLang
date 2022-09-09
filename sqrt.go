03a3beb56482775083361bf077bbabf7e1a7eb8f
Joseph Wildey <joewildey@outlook.com>
2022-09-09 13:46:26 -0400

    Update sqrt.go

--- sqrt.go
+++ sqrt.go
@@ -20,18 +20,22 @@ func Sqrt(x float64) float64 {
 
     }
 
-    // returns the estimated square root
+    // returns the estimated square root as a whole number
     return math.Floor(z)
 
 }
 
 func main() {
 
+    // This variable will be the number the user wants to get the square root of
     var usrNum float64 
 
+    // This line asks the user for input
     fmt.Println("Please enter a number")
 
+    // Captures user input
     fmt.Scanln(&usrNum)
 
+    // Outputs the solution
     fmt.Println(Sqrt(usrNum))
 }
